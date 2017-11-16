c699ff57bdf9a18a5342e6b381f7fdee61c151eb
Todsphol <theaddza11@gmail.com>
2017-11-17 01:10:51 +0700

    First commit in project

---
+++ app/src/main/java/th/co/todsphol/add/projectone/MainActivity.kt
@@ -0,0 +1,12 @@
+package th.co.todsphol.add.projectone
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
