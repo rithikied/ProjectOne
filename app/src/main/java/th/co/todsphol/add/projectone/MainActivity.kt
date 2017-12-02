19c752cae7cc374cdf5d6540fee50922aad1b1a7
Todsphol <theaddza11@gmail.com>
2017-12-02 21:36:19 +0700

    Set Fragment in Activity

--- app/src/main/java/th/co/todsphol/add/projectone/MainActivity.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFragment()
+
+    }
+    fun initFragment() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, FirstFragment.newInstance())
+                .commit()
     }
 }
