0afacd54363de5871e594bf8d032b64139e8a636
Todsphol <theaddza11@gmail.com>
2017-12-02 21:31:43 +0700

    Add fragment and Create newInstannce

---
+++ app/src/main/java/th/co/todsphol/add/projectone/FirstFragment.kt
@@ -0,0 +1,27 @@
+package th.co.todsphol.add.projectone
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class FirstFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_first, container, false)
+    }
+
+    companion object {
+        fun newInstance() : Fragment {
+            val bundel = Bundle()
+            val fragment = FirstFragment()
+            fragment.arguments = bundel
+            return fragment
+        }
+    }
+
+}
