d5fc0b834b0d62d32dc76755de38e09bf34b9af7
Todsphol <theaddza11@gmail.com>
2017-12-03 17:20:38 +0700

    call Toolbar come in using

--- app/src/main/java/th/co/todsphol/add/projectone/MainActivity.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/MainActivity.kt
@@ -2,14 +2,26 @@ package th.co.todsphol.add.projectone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var myToolbar : Toolbar
+    private lateinit var tvTitle : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initFragment()
+        setToolbar()
 
+    }
+    fun setToolbar() {
+        myToolbar = findViewById(R.id.toolbar)
+        tvTitle = findViewById(R.id.tv_toolbar_title)
+        setSupportActionBar(myToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+        tvTitle.text = "Hellow World"
     }
     fun initFragment() {
         supportFragmentManager.beginTransaction()
