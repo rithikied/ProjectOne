efd8386be24affa674ce96682dec4e2e03ce322c
Todsphol <theaddza11@gmail.com>
2017-12-11 22:46:42 +0700

    button click to function

--- app/src/main/java/th/co/todsphol/add/projectone/fragment/FirstFragment.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/fragment/FirstFragment.kt
@@ -1,13 +1,13 @@
 package th.co.todsphol.add.projectone.fragment
 
 
+import android.app.ActionBar
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import kotlinx.android.synthetic.main.fragment_first.*
 import th.co.todsphol.add.projectone.R
 import th.co.todsphol.add.projectone.activity.MainActivity
 
@@ -17,15 +17,18 @@ class FirstFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_first, container, false)
+        initButton(view)
+        return view
+
+    }
+    private fun initButton(view: View) {
         val btn = view.findViewById<Button>(R.id.btn_next)
         btn.setOnClickListener {
             getMainActivity().changeFragment(SecondFragment.newInstance())
         }
-        return view
-
     }
 
-    fun getMainActivity() : MainActivity {
+    private fun getMainActivity() : MainActivity {
         return activity as MainActivity
     }
     companion object {
--- app/src/main/java/th/co/todsphol/add/projectone/fragment/SecondFragment.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/fragment/SecondFragment.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
 
 import th.co.todsphol.add.projectone.R
 
@@ -15,7 +16,6 @@ class SecondFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_second, container, false)
-
         return view
     }
 
