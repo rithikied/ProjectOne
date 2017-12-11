2631c21bbf098c0a266861d003d5a6a776fec05a
Todsphol <theaddza11@gmail.com>
2017-12-11 21:56:52 +0700

    Create new fragment

---
+++ app/src/main/java/th/co/todsphol/add/projectone/fragment/SecondFragment.kt
@@ -0,0 +1,20 @@
+package th.co.todsphol.add.projectone.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import th.co.todsphol.add.projectone.R
+
+class SecondFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater!!.inflate(R.layout.fragment_second, container, false)
+    }
+}
