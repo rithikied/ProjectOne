c53ba5dc5825da9e54e0764c2f4f67c2bd3eb80e
Todsphol <theaddza11@gmail.com>
2018-01-03 21:33:24 +0700

    Add new activity and setOnClick button newregister

---
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/NewRegisterActivity.kt
@@ -0,0 +1,13 @@
+package th.co.todsphol.add.projectone.activity
+
+import android.app.Activity
+import android.os.Bundle
+import th.co.todsphol.add.projectone.R
+
+class NewRegisterActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new__register)
+    }
+}
--- app/src/main/java/th/co/todsphol/add/projectone/fragment/FirstFragment.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/fragment/FirstFragment.kt
@@ -1,7 +1,9 @@
 package th.co.todsphol.add.projectone.fragment
 
 
+import android.content.Intent
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,27 +14,37 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import th.co.todsphol.add.projectone.R
 import th.co.todsphol.add.projectone.activity.FirstPageActivity
+import th.co.todsphol.add.projectone.activity.NewRegisterActivity
 
 class FirstFragment : Fragment() {
 
-    @BindView(R.id.edt_phone_number) lateinit var edtPhone : EditText
-    @BindView(R.id.edt_password) lateinit var edtPassword : EditText
-    @BindView(R.id.btn_new_register) lateinit var newRegister : Button
-    @BindView(R.id.btn_register) lateinit var login : Button
+    @Nullable @BindView(R.id.edt_phone_number) lateinit var edtPhone: EditText
+    @Nullable @BindView(R.id.edt_password) lateinit var edtPassword: EditText
+    @Nullable @BindView(R.id.btn_new_register) lateinit var newRegister: Button
+    @Nullable @BindView(R.id.btn_register) lateinit var login: Button
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view = inflater!!.inflate(R.layout.fragment_first, container, false)
-        ButterKnife.bind(view)
+        ButterKnife.bind(this, view)
         getMainActivity().supportActionBar?.hide()
+        onClickNewRegister()
+
         return view
     }
 
-    private fun getMainActivity() : FirstPageActivity {
+    fun onClickNewRegister() {
+        newRegister.setOnClickListener {
+            getMainActivity().changeFragment(SecondFragment.newInstance())
+        }
+    }
+
+
+    private fun getMainActivity(): FirstPageActivity {
         return activity as FirstPageActivity
     }
 
     companion object {
-        fun newInstance() : Fragment{
+        fun newInstance(): Fragment {
             val bundle = Bundle()
             val fragment = FirstFragment()
             fragment.arguments = bundle
