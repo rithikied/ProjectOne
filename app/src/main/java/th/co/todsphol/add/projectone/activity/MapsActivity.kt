3a49d077f3e6164736c52ce8cf423c3ea9cabbf8
Todsphol <theaddza11@gmail.com>
2018-01-06 14:17:32 +0700

    Set Camera zoom to custom location

--- app/src/main/java/th/co/todsphol/add/projectone/activity/MapsActivity.kt
+++ app/src/main/java/th/co/todsphol/add/projectone/activity/MapsActivity.kt
@@ -1,22 +1,23 @@
 package th.co.todsphol.add.projectone.activity
 
-
-import android.content.Intent
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.SyncStateContract
 import android.support.annotation.Nullable
 import android.widget.Button
 import butterknife.BindView
+import com.google.android.gms.maps.CameraUpdateFactory
 
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import th.co.todsphol.add.projectone.R
-import android.R.attr.button
-import android.annotation.SuppressLint
-import android.view.View
+
+
+
+
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -43,8 +44,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.uiSettings.isMyLocationButtonEnabled = true
         val chagwattana = LatLng(13.903890, 100.528437)
-//        mMap.addMarker(MarkerOptions().position(chagwattana).title("Changwattana"))
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(chagwattana))
+        mMap.addMarker(MarkerOptions().position(chagwattana).title("Changwattana"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(chagwattana))
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15.0f))
+
     }
+
 
 }
